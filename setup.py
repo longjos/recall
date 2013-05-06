c9503b8486da1b1348a8f115372ebccb4034028c
Doug Hurst <dalan.hurst@gmail.com>
2013-05-05 21:16:30 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '1.0.0-pre'
+setup(
+    name='recall',
+    version=version,
+    description='CQRS Library for Python',
+    author='Doug Hurst',
+    author_email='dalan.hurst@gmail.com',
+    maintainer='Doug Hurst',
+    license='MIT',
+    url='https://github.com/dalanhurst/recall',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    download_url='http://pypi.python.org/packages/source/r/recall/recall-%s.tar.gz' % version,
+    include_package_data=True,
+    package_data={'': ['requirements.txt']},
+    install_requires=[
+        item for item in
+        open("requirements.txt").read().split("\n")
+        if item],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python']
+)
